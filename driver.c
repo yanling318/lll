e3c08f472211f9f08b0297d6c4b841fb0f61fde2
yanling318 <boyyanling@gmail.com>
2018-02-04 17:12:01 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -5,26 +5,24 @@ int main(){
 	init_graphics();
 
 	//Draw 3 rectangles and 1 pixel instantly
-	draw_rect(100, 100, 50, 100, (color_t) MAKE_COLOR(30, 0, 0)); //red
-	draw_rect(150, 100, 50, 50, MAKE_COLOR(15, 50, 10)); //blue-ish
-	draw_rect(0, 0, 100, 100, MAKE_COLOR(30, 60, 30)); //white
-	draw_pixel(200, 250, MAKE_COLOR(0, 60, 0)); //green
+	draw_rect(100, 100, 50, 100, 10); //red
+	draw_rect(150, 100, 50, 50,15); //blue-ish
+	draw_rect(0, 0, 100, 100,60); //white
+	draw_pixel(200, 250, 70); //green
 
 	sleep_ms(5000); //test the sleep function
-	draw_rect(-150, 300, 80000, 300, MAKE_COLOR(23, 54, 1)); //yellow-ish rectangle mostly off-screen; almost looks like a line
+	draw_rect(-150, 300, 80000, 300, 40); //yellow-ish rectangle mostly off-screen; almost looks like a line
 
 	sleep_ms(5000);
 	clear_screen(); //test the function that clears the screen
 	sleep_ms(2000);
 
-	draw_rect(300, 300, 100, 50, MAKE_COLOR(10, 10, 10)); //maroon/brown/purple
+	draw_rect(300, 300, 100, 50,70); //maroon/brown/purple
 	sleep_ms(1000);
-	draw_rect(100, 150, 200, 200, MAKE_COLOR(30, 50, 5)); //yellow/gold
+	draw_rect(100, 150, 200, 200, 40); //yellow/gold
 	sleep_ms(500);
 
-	//test a 500ms interval between text, centered in large rectangle
-
-	while(getkey() != 'c'){} //test to see if input works; basically acts as an exit for the user
+	//test a 500ms interval between text, centered in large rectang
 
 	exit_graphics(); //restore default settings and exit from graphics library
 }
