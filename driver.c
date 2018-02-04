3e9dc3e394549a1e1279bb9c249c829d4d68ecbc
yanling318 <boyyanling@gmail.com>
2018-02-04 18:50:38 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "library.c"
 typedef unsigned short color_t;
+void init_graphics();
+void exit_graphics();
+void clear_screen();
+char getkey();
+void sleep_ms(long ms);
+void draw_pixel(int x, int y, color_t color);
+void draw_rect(int x1, int y1, int width, int height, color_t c);
+void fill_circle(int x, int y, int r, color_t c);
+void draw_text(int x, int y, const char *text, color_t c);
 
 int main (int argc, char** argv)
 {
