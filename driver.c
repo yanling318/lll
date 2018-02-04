1c7aca291e028a3b43d81f7e87eedb6b7ec06e6d
yanling318 <boyyanling@gmail.com>
2018-02-04 16:28:03 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,63 +1,38 @@
-#include<stdio.h>
-#include"library.c"
-#define BLUE  30
-#define GREEN 2016
-#define RED   63488
-int main(void){
-    printf("start\n");
-    sleep_ms(1500);
-    clear_screen();
-    
-    init_graphics();
-    int i=0;
-    for( i; i<480; i++){
-        draw_pixel(i, 50, BLUE );
-        draw_pixel(i, 60, GREEN);
-        draw_pixel(i, 70, RED  );
-    }
-    sleep_ms(100);
-    for(i=0; i<640; i++){
-        draw_pixel(50, i, RED  );
-        draw_pixel(60, i, GREEN);
-        draw_pixel(70, i, BLUE );
-    }
-    sleep_ms(250);
-    
-    draw_rect(100,100,30,30, RED  );
-    sleep_ms(250);
-    draw_rect(110,110,10,10, GREEN);
-    sleep_ms(250);
-    draw_rect( 90, 90,50,50, BLUE );
-    
-    sleep_ms(1000);
-    
-    fill_circle(200,200, 50, BLUE);
-    
-    draw_rect(150,150,100,100,RED);
-    
-    sleep_ms(250);
-    fill_circle(200,200,20,GREEN);
-    
-    sleep_ms(250);
-    fill_circle(400,200,10, GREEN);
-    
-    clear_screen();
-    int q=0;
-    int z=0;
-    while(getkey()==NULL&&q<600){
-        for(q=0; q<480; q++){
-            draw_pixel(q,z,GREEN);
-        }
-        z++;
-        sleep_ms(100);
-    }
-    clear_screen();
-    
-    draw_text(10,10, "Testing writing text", RED);
-    draw_text(50,10, "This is BLUE", BLUE);
-    sleep_ms(250);
-    exit_graphics();
-    
-    printf("done\n");
-    
-    return 0;
+#include "library.c"
+
+int main(){
+	//Initialize the graphics library
+	init_graphics();
+
+	//Draw 3 rectangles and 1 pixel instantly
+	draw_rect(100, 100, 50, 100, (color_t) MAKE_COLOR(30, 0, 0)); //red
+	draw_rect(150, 100, 50, 50, MAKE_COLOR(15, 50, 10)); //blue-ish
+	draw_rect(0, 0, 100, 100, MAKE_COLOR(30, 60, 30)); //white
+	draw_pixel(200, 250, MAKE_COLOR(0, 60, 0)); //green
+
+	sleep_ms(5000); //test the sleep function
+
+	draw_text(0, 400, "hello world!", MAKE_COLOR(31, 63, 31)); //white
+	draw_text(-100, 300, "sample text wow?!", MAKE_COLOR(15, 31, 15)); //grey text mostly to the left of the display
+	draw_rect(-150, 300, 80000, 300, MAKE_COLOR(23, 54, 1)); //yellow-ish rectangle mostly off-screen; almost looks like a line
+
+	sleep_ms(5000);
+	clear_screen(); //test the function that clears the screen
+	sleep_ms(2000);
+
+	draw_rect(300, 300, 100, 50, MAKE_COLOR(10, 10, 10)); //maroon/brown/purple
+	sleep_ms(1000);
+	draw_rect(100, 150, 200, 200, MAKE_COLOR(30, 50, 5)); //yellow/gold
+	sleep_ms(500);
+
+	//test a 500ms interval between text, centered in large rectangle
+	draw_text(148, 242, "this ", MAKE_COLOR(31, 0, 0)); //red
+	sleep_ms(500);
+	draw_text(188, 242, "is ", MAKE_COLOR(0, 63, 0)); //green
+	sleep_ms(500);
+	draw_text(212, 242, "cool!", MAKE_COLOR(0, 0, 31)); //blue
+
+	while(getkey() != 'c'){} //test to see if input works; basically acts as an exit for the user
+
+	exit_graphics(); //restore default settings and exit from graphics library
+}
