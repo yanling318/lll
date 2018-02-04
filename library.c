9506de1878f2f5dd5a2611984073506637596866
yanling318 <boyyanling@gmail.com>
2018-02-04 17:04:43 -0500

    Update library.c

--- library.c
+++ library.c
@@ -1,166 +1,240 @@
-
-#include "iso_font.h" //Apple's supplied font
-
-#include <unistd.h> //Contains STDIN and STDOUT constants
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <linux/fb.h>
 #include <fcntl.h>
 #include <termios.h>
+#include <unistd.h>
 
-#include <linux/fb.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-
-#define MAKE_COLOR(r, g, b) ((color_t) (r << 11) | (g << 5) | (b))
-
+/* Global variables */
+int fd;
+unsigned short *fb_ptr;
+unsigned long size;
+unsigned long x_virtual_len;
+unsigned long y_virtual_len;
 typedef unsigned short color_t;
 
-int framebuffer_desc; //The framebuffer device file descriptor
-color_t* framebuffer; //The actual address of the framebuffer in memory
-int size_of_display; //Size of the display's space when mapping the framebuffer to our address space
-struct termios terminal_settings; //Settings of the terminal; mostly ICANON and ECHO will be used
-struct fb_var_screeninfo screen_var_info;
-struct fb_fix_screeninfo screen_fix_info;
-
+/* Prototypes for our nine library functions. */
+void init_graphics();
+void exit_graphics();
 void clear_screen();
+char getkey();
+void sleep_ms(long ms);
+void draw_pixel(int x, int y, color_t color);
+void draw_rect(int x1, int y1, int width, int height, color_t c);
+void fill_circle(int x, int y, int r, color_t c);
+void draw_text(int x, int y, const char *text, color_t c);
+void draw_character(int x, int y, color_t c, int ascii_val);
 
-void init_graphics(){
-	framebuffer_desc = open("/dev/fb0", O_RDWR); //Open the framebuffer and get the file descriptor
+/**
+ * In this function all necessary work to initialize the graphics library is done:
+ * There are four steps.
+ */
+void init_graphics()
+{
+   /* 1. Open the graphics device /dev/fb0 (frame buffer) using the open syscall. */
+   /* 2. Screen resolution detection using the ioctl suyscall. */
+   /* 3. Memory mapping using the mmap syscall. */
+   /* 4. Disable display from keyboard. */
+   struct fb_var_screeninfo var_info;
+   struct fb_fix_screeninfo fix_info;
+   struct termios term;
 
-	ioctl(framebuffer_desc, FBIOGET_VSCREENINFO, &screen_var_info);
-	ioctl(framebuffer_desc, FBIOGET_FSCREENINFO, &screen_fix_info);
+   fd = open("/dev/fb0", O_RDWR);
+   ioctl(fd, FBIOGET_VSCREENINFO, &var_info);
+   ioctl(fd, FBIOGET_FSCREENINFO, &fix_info);
+   x_virtual_len = var_info.xres_virtual;
+   y_virtual_len = var_info.yres_virtual;
+   size = fix_info.line_length;
 
-	ioctl(STDIN_FILENO, TCGETS, &terminal_settings); //Get the current terminal settings
-	terminal_settings.c_lflag &= ~ICANON; //Switch ICANON off
-	terminal_settings.c_lflag &= ~ECHO; //Switch ECHO off
-	ioctl(STDIN_FILENO, TCSETS, &terminal_settings); //Set the new terminal settings
+   fb_ptr = (unsigned short *)mmap(NULL, x_virtual_len * size,
+   	                        PROT_WRITE, MAP_SHARED, fd, 0);
 
-	size_of_display = screen_var_info.yres_virtual * screen_fix_info.line_length; //Set the size of the display
-
-	framebuffer = (color_t*) mmap(NULL, size_of_display, PROT_WRITE, MAP_SHARED, framebuffer_desc, 0); //Maps the frame buffer in memory
-
-	clear_screen(); //Make sure the screen is cleared so there's no text in the background
+   ioctl(STDIN_FILENO, TCGETS, &term);
+   term.c_lflag &= ~ICANON; //disable canonical mode
+   term.c_lflag &= ~ECHO; //disable ECHO
+   ioctl(STDIN_FILENO, TCSETS, &term);
 }
 
-//Exit the graphics library and reset the settings of the frame to its default settings, including unmapping
-void exit_graphics(){
-	clear_screen(); //Clear the screen when we're done
+/* Undo whatever it is that needs to be cleaned up before the program exists. */
+void exit_graphics()
+{
+   /* Unmap file from address space memory. */
+   munmap(fb_ptr, y_virtual_len * size);
 
-	terminal_settings.c_lflag |= ICANON; //Switch ICANON on again
-	terminal_settings.c_lflag |= ECHO; //Switch ECHO on again
-	ioctl(STDIN_FILENO, TCSETS, &terminal_settings); //Set these new settings
+   /* Activate key display by re-enabling canonical mode and ECHO. */
+   /* '1' for the file descriptor parameter is reserved for stdout. */
+   struct termios term;
+   ioctl(STDIN_FILENO, TCGETS, &term);
+   term.c_lflag |= ICANON;
+   term.c_lflag |= ECHO;
+   ioctl(STDIN_FILENO, TCSETS, &term);
 
-	munmap(framebuffer, size_of_display); //Unmap the framebuffer from our address space
-	close(framebuffer_desc); //Close the file descriptor of the framebuffer
+   /* Close the framebuffer file. */
+   close(fd);
 }
 
-//Clear the screen of all its current color
-void clear_screen(){
-	write(STDOUT_FILENO, "\033[2J", 4); //Tells the terminal to clear itself by printing out to the standard output; the first part of the string is an octal escape sequence
+/* Clear the screen using the write syscall. */
+void clear_screen()
+{
+   /* Using the ANSI escape code "\033[2J". */
+   write(1, "\033[2J", 8);
 }
 
-//Check to see if there is a character waiting in the buffer.
-//If a character is there, return it to the caller.
-char getkey(){
-	fd_set rfds; //File descriptor set manages which file descriptors are being used; is passed into select()
-	FD_ZERO(&rfds); //Clear the above set just to be safe
-	FD_SET(0, &rfds); //Add file descriptor 0 (stdin) to the descriptor set
+/* Read character inputs from user. */
+char getkey()
+{
+   /**
+    * The read() syscall is blocking and will cause our program
+    * to not draw unless the user has typed something. So we need
+    * to use the non-blocking SELECT syscall to monitor
+    * when read() is ready, then we perform read().
+    */
+   char input_key;
+   fd_set rfds;
+   struct timeval tv;
 
-	struct timeval time_wait;
-	time_wait.tv_sec = 5; //Wait 25seconds
-	time_wait.tv_usec = 0; //Wait 0 microseconds
-	int char_in_buffer = select(STDIN_FILENO+1, &rfds, NULL, NULL, &time_wait); //Check 1 device (file descriptor 0, a.k.a. stdin); first parameter must be highest file descriptor device + 1
+   /* Watch stdin (fd 0) to see when it has input. */
+   FD_ZERO(&rfds);
+   FD_SET(0, &rfds);
 
-	char buffered_char = '\0';
-
-	if(char_in_buffer){ //There is a character in the stdin buffer
-		read(0, &buffered_char, sizeof(buffered_char)); //Read 1 byte of data from file descriptor 0 (stdin) and store it in buffered_char
-	}
-
-	return buffered_char;
+   /* Wait up to five seconds. */
+   tv.tv_sec = 5;
+   tv.tv_usec = 0;
+   int select_r = select(STDIN_FILENO+1, &rfds, NULL, NULL, &tv);
+   if (select_r > 0)
+   {
+   	read(0, &input_key, sizeof(input_key));
+   }
+   return input_key;
 }
 
-//Sleep for a specified number of milliseconds
-void sleep_ms(long ms){
-	//Cannot sleep for negative time
-	if(ms < 0){
-		return;
-	}
-
-	//Create the struct to hold the nanoseconds we're sleeping for
-	struct timespec sleep_time_struct;
-	sleep_time_struct.tv_sec = 0; //Seconds to sleep
-
-	//999,999,999 ns (1 second) is the longest you can nanosleep() at a given time,
-	//so if the input number is less than that, sleeping can be done in one call
-	if(ms <= 999){
-		sleep_time_struct.tv_nsec = ms*1000000;
-		nanosleep(&sleep_time_struct, NULL);
-	} else{ //else, it will require a loop that calls nanosleep() several times
-		while(ms > 0){ //There is still time left to sleep
-			ms -= 999;
-			if(ms > 0){
-				sleep_time_struct.tv_nsec = 999000000; //There is some time left over after subtracting 999,000,000 ns, so we can sleep for that time
-			} else{
-				sleep_time_struct.tv_nsec = (ms + 999)*1000000; //There is less than 999,000,000 ns left, so add it back on for now and sleep for that specific amount of nanoseconds
-			}
-			nanosleep(&sleep_time_struct, NULL);
-		}
-	}
+/**
+ * Using the syscall nanosleep() make the program sleep between frames
+ * of graphics being drawn. We will sleep for a specified number of ms.
+ */
+void sleep_ms(long ms)
+{
+   /**
+    * We do not need to worry about the call being interrupted
+    * and so the second parameter to nanosleep() is NULL.
+    */
+   struct timespec ts;
+   ts.tv_sec = 0;
+   ts.tv_nsec = ms * 1000000;
+   nanosleep(&ts, NULL);
 }
 
-//Draw 1 pixel located at the coordinate (x, y) with the a specified color
-void draw_pixel(int x, int y, color_t color){
-	//Do not try drawing a pixel off screen/out of bounds; it's unncessary
-	if(x < 0 || y < 0 || x > screen_var_info.xres_virtual || y > screen_var_info.yres_virtual){
-		return;
-	}
+/**
+ * This is the main drawing code; where the work is actually done.
+ * We want to set a pixel at coordinate (x, y) to the specified color.
+ * The frame buffer will be stored in row-major order, measuring the
+ * first row starts at offset 0 and then that is followed by the second
+ * row of pixels, and so on.
+ */
+void draw_pixel(int x, int y, color_t color)
+{
+   /**
+    * fb_ptr points to the beginning of a one-dimensional array,
+    * while the screen is a two-dimensional array. We need to jump
+    * to (x, y) so the program needs to know the length of each line
+    * - regarding the x-axis. And the y-offset.
+    * E.g (x, y) = (1, 2), index = ?
+    */
+   if (x < 0 || x >= x_virtual_len || y < 0 || y >= y_virtual_len)
+   {
+      /* Out of bounds exception handling. */
+      return;
+   }
 
-	int offset = (y * screen_var_info.xres_virtual) + x; //Set the offset in order to later modify that address using pointer arithmetic
-	color_t* pixel = framebuffer + offset; //Navigate to necessary address
-	*pixel = color; //Set the color of the necessary pixel
+   /* Use pointer arithmetic to set the pixel to the specified color. */
+   unsigned long vertical = (size/2) * y;
+   unsigned long horizontal = x;
+   unsigned short *ptr = ( fb_ptr + vertical + horizontal);
+   *ptr = color;
 }
 
-//Draw a rectangle located at (x, y) with specified height, width, and unsigned 16-bit color
-void draw_rect(int x1, int y1, int width, int height, color_t c){
-	//Return if the rectangle will be completely off screen
-	if(((x1+width) < 0 && (y1+height) < 0) || (x1 > screen_var_info.xres_virtual && y1 > screen_var_info.yres_virtual)){
-		return;
-	}
-
-	int i = 0;
-
-	for(i = 0; i < width; i++){ //Draw top side of rectangle
-		draw_pixel(x1++, y1, c);
-	}
-
-	for(i = 0; i < height; i++){ //Draw right side of rectangle
-		draw_pixel(x1, y1++, c);
-	}
-
-	for(i = 0; i < width; i++){ //Draw bottom side of rectangle
-		draw_pixel(x1--, y1, c);
-	}
-
-	for(i = 0; i < height; i++){ //Draw left side of rectangle
-		draw_pixel(x1, y1--, c);
-	}
+/**
+ * Using draw_pixel(), make a non-filled rectangle with corners
+ * (x1, y1), (x1+width, y1), (x1+width, y1+height), (x1, y1+height).
+ */
+void draw_rect(int x1, int y1, int width, int height, color_t c)
+{
+   int x, y;
+   for (x = x1; x < x1+width; x++)
+   {
+      for (y = y1; y < y1+height; y++)
+      {
+         if (x == x1 || x == x1+width-1)
+         {
+            draw_pixel(x, y, c);
+         }
+         if (y == y1 || y == y1+height-1)
+         {
+            draw_pixel(x, y, c);
+         }
+      }
+   }
 }
 
-//Draw a given character found in the iso_font array with given color c at location (x, y)
-void draw_char(int x, int y, const char character, color_t c){
-	int i = 0;
-	int j = 0;
-
-	for(i = 0; i < 16; i++){ //Iterate through the rows
-		int curr_row = iso_font[character*16 + i]; //Get the pixel data for row i		
-
-		for(j = 0; j < 8; j++){ //Iterate left through right in this specific row
-			int draw_pixel_boolean = curr_row & 0x01; //AND the current row with 0x80 (1000 0000) to get the most significant bit
-			curr_row >>= 1; //Shift the current row value right by 1 so we can continue to grab the least significant bit
-			if(draw_pixel_boolean){ //If our AND'd bit is 1, draw the pixel there
-				draw_pixel(x + j, y + i, c);
-			}
-		}
-	}
+/**
+ * Use the midpoint circle algorithm to draw a circle at (x, y)
+ * with radius r filled in with the specified color.
+ */
+void fill_circle(int x, int y, int r, color_t c)
+{
+   /**
+    * The mid point circle algorithm only draws the circumference
+    * of a circle given a length of the radius, r. So we modify the
+    * algorithm to run multiple times while iterating over the radius
+    * length decreasing the radius length by 1 on every iteration.
+    */
+   int i;
+   for (i = r; i > 0; i--)
+   {
+      int xx = i;
+      int yy = 0;
+      int x0 = x;
+      int y0 = y;
+      int decision_over_2 = 1 - xx;
+      /* Draw the circumference of the circle for radius length, r. */
+      while (yy <= xx)
+      {
+         draw_pixel( xx+x0, yy+y0, c);
+         draw_pixel( yy+x0, xx+y0, c);
+         draw_pixel( -xx+x0, yy+y0, c);
+         draw_pixel( -yy+x0, xx+y0, c);
+         draw_pixel( -xx+x0, -yy+y0, c);
+         draw_pixel( -yy+x0, -xx+y0, c);
+         draw_pixel( xx+x0, -yy+y0, c);
+         draw_pixel( yy+x0, -xx+y0, c);
+         yy++;
+         if (decision_over_2 <= 0)
+         {
+            decision_over_2 += 2 * yy + 1;
+         }
+         else
+         {
+            xx--;
+            decision_over_2 += 2 * (yy - xx) +1;
+         }
+      }
+   }
 }
 
+/**
+ * Draw the string with the specified color at the starting location
+ * (x, y) -- this is the upper left corner of the first letter.
+ * This function simply loops over all the characters in the string.
+ */
+void draw_text(int x, int y, const char *text, color_t c)
+{
+   const char *temp_ptr;
+   int offset = 0;
+   for (temp_ptr = text; *temp_ptr != '\0'; temp_ptr++)
+   {
+      draw_character(x, y+offset, c, *temp_ptr);
+      offset += 8;
+   }
 }
